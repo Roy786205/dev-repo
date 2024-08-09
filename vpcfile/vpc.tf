973d159e81f9c1242d9301f5af3da51ebb869760
rohit <rohitpatil40234023@gmail.com>
2024-08-09 16:58:33 +0530

    --- Auto Git Commit ---

--- vpcfile/vpc.tf
+++ vpcfile/vpc.tf
@@ -1,62 +1,62 @@
-#Provider for authentication
-# provider "aws" {
-#     region = var.region
-# }
+Provider for authentication
+provider "aws" {
+    region = var.region
+}
 
 
-# # Terraform for S3 Backend
-# terraform {
-#     backend "s3" {
-#     bucket = "prod-terrraform.tfstate"
-#     key    = "terraform.tfstate"
-#     region = "us-west-2"
-#     }
-# }
+# Terraform for S3 Backend
+terraform {
+    backend "s3" {
+    bucket = "prod-terrraform.tfstate"
+    key    = "terraform.tfstate"
+    region = "us-west-2"
+    }
+}
 
-# #Data block for SG
-# data "aws_security_group" "server" {
-#     name = "default"
-# }
+#Data block for SG
+data "aws_security_group" "server" {
+    name = "default"
+}
 
-# # resource block for server creation
-# resource "aws_instance" "server_1" {
-#     ami = var.ami
-#     instance_type = var.instance_type
-#     key_name = var.key_name
-#     tags = var.tags
-#     vpc_security_group_ids = [data.aws_security_group.server.id]
-# }
+# resource block for server creation
+resource "aws_instance" "server_1" {
+    ami = var.ami
+    instance_type = var.instance_type
+    key_name = var.key_name
+    tags = var.tags
+    vpc_security_group_ids = [data.aws_security_group.server.id]
+}
 
-# #variable block calling all values
-# variable "region" {
-#     default = "us-west-2"
-#     description = "region calling"
-# }
+#variable block calling all values
+variable "region" {
+    default = "us-west-2"
+    description = "region calling"
+}
 
-# variable "ami" {
-#     default = "ami-0f7197c592205b389"
-#     description = "calling ami"
-# }
+variable "ami" {
+    default = "ami-0f7197c592205b389"
+    description = "calling ami"
+}
 
-# variable "instance_type" {
-#   default = "t2.micro"
-#   description = "calling instance type"
-# }
+variable "instance_type" {
+  default = "t2.micro"
+  description = "calling instance type"
+}
 
-# variable "key_name" {
-#     default = "windows_global_key"
-#     description = "calling key-pair"
-# }
+variable "key_name" {
+    default = "windows_global_key"
+    description = "calling key-pair"
+}
 
-# variable "tags" {
-#     type = map
-#     default = {
-#     Name = "Development-server"
-#     Enviorment = "staging"
-#     Owner = "jarvis"
-#     }
-# }
+variable "tags" {
+    type = map
+    default = {
+    Name = "Development-server"
+    Enviorment = "staging"
+    Owner = "jarvis"
+    }
+}
 
-# output "test" {
-#     value = data.aws_security_group.server.id
-# }
+output "test" {
+    value = data.aws_security_group.server.id
+}
