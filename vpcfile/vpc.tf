ddba4eb6f6c8bfd3900ce23f41a286f0461f5693
rohit <rohitpatil40234023@gmail.com>
2024-08-09 17:56:55 +0530

    .

--- vpcfile/vpc.tf
+++ vpcfile/vpc.tf
@@ -23,7 +23,7 @@ resource "aws_subnet" "default_az1" {
 resource "aws_route_table" "example" {
 vpc_id = aws_vpc.default.id
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
 
